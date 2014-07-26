b1615c587659a8e16aae781d98287af59b71c177
Tanuja Prasad <tanuja.tp@gmail.com>
2014-07-25 22:06:58 -0400

    Update run_analysis.R
    Corrections to the tidy data set.

--- run_analysis.R
+++ run_analysis.R
@@ -1,51 +1,57 @@
 
+run_analysis <- function ( data_dir = "UCI HAR Dataset" ) {
 
-alldata <- data.frame()
+	alldata <- data.frame()
 
-for ( dir in c("train", "test") ) {
-	file_prefix <- paste("./UCI HAR Dataset/",dir,"/", sep="")
-	x_data <- read.csv(paste(file_prefix, "x_", dir, ".txt", sep=""))
-	y_data <- read.csv(paste(file_prefix, "y_",dir,".txt", sep=""))
-	subj_data <- read.csv(paste(file_prefix, "subject_",dir,".txt", sep=""))
+	## read structure data
+	features <- read.table(paste(data_dir,"/features.txt",sep=""),sep=" ",col.names=c("feature_id","feature"))
+	activities <- read.table(paste(data_dir,"/activity_labels.txt",sep=""),sep=" ",col.names=c("activity_id","activity"))
+ 
+    test_subj <- read.table(paste(data_dir,"/test/subject_test.txt",sep=""),sep=" ",col.names=c("id")) 
+    train_subj <- read.table(paste(data_dir,"/train/subject_train.txt",sep=""),sep=" ",col.names=c("id")) 
 
-	colnames(x_data) <- c("obs_data")
-	x_data[,"activity"] <- y_data$X5
-	if ( dir == "train" ) {
-		x_data[,"subject"] <- subj_data$X1
-		x_data[,"training"] <- c(rep(1, nrow(x_data)))
-	} else {
-		x_data[,"subject"] <- subj_data$X2
-		x_data[,"training"] <- c(rep(0, nrow(x_data)))
+    test_act <- read.table(paste(data_dir,"/test/y_test.txt",sep=""),sep=" ",col.names=c("id"))
+    train_act <- read.table(paste(data_dir,"/train/y_train.txt",sep=""),sep=" ",col.names=c("id"))
+	
+	## need to read only the mean and std deviation features
+	## the files containing the measurements is a fixed-width file, with each field being 16 chars wide
+	## so in order to extract only the mean and std deviation fields we need to know their positions
+	## Note that -16 indicates a field that needs to be skipped
+	fpos <- rep(-16,nrow(features))
+	features_sub <- subset(features, grepl("mean|std",feature,ignore.case=TRUE))
+	fpos[features_sub$feature_id] = 16;
+	test_meas <- read.fwf(paste(data_dir,"/test/x_test.txt",sep=""),widths=fpos,col.names=features_sub$feature) 
+    train_meas <- read.fwf(paste(data_dir,"/train/x_train.txt",sep=""),widths=fpos,col.names=features_sub$feature) 
+
+	## combine all data into one dataframe "alldata". 
+    alldata <- rbind(test_meas,train_meas)
+	
+	## Use the combined activity data to compute mean by activity
+    activity_data <- rbind(test_act,train_act)
+	b <- by(alldata, activity_data$id, colMeans)  ## returns an object of class "b" which is a list of dataframes
+	
+	## combine all the means from the dataframes into one dataframe
+	tidydata <- data.frame( b[[1]] )  ## initializing with the first dataframe
+	for ( n in 2:length(b) ) {
+		tidydata <- cbind( tidydata, data.frame(b[[n]]) )
+	}
+
+	
+	## Use the combined subject data to compute mean by subject
+	subject_data <- rbind(test_subj,train_subj)
+	b <- by(alldata, subject_data$id, colMeans)  ## returns an object of class "b" which is a list of dataframes
+
+	## combine all the means from the dataframes into one dataframe
+	for ( n in 1:length(b) ) {
+	    tidydata <- cbind( tidydata, data.frame(b[[n]]) )
 	}
 	
-	file_prefix <- paste(file_prefix, "Inertial Signals/", sep="")
-	
-	total_acc_z <- read.csv( paste(file_prefix, "total_acc_z_",dir, ".txt", sep="") )
-	total_acc_y <- read.csv( paste(file_prefix, "total_acc_y_",dir, ".txt", sep="") )
-	total_acc_x <- read.csv( paste(file_prefix, "total_acc_x_",dir, ".txt", sep="") )
+	# give user-friendly columns names
+	subject_names <- paste( "subject", unique(subject_data$id), sep="" ) ## in order to preserve the order
+	activity_names <- tolower( activities$activity[unique(activity_data$id)] ) ## in order to preserve the order
+	colnames(tidydata) <- c(activity_names, subject_names)	
 
-	x_data[,"total_acc_z"] <-total_acc_z[1]
-	x_data[,"total_acc_y"] <-total_acc_y[1]
-	x_data[,"total_acc_x"] <-total_acc_x[1]
-		
-	body_acc_z <- read.csv( paste(file_prefix, "body_acc_z_",dir, ".txt", sep="") )
-	body_acc_y <- read.csv( paste(file_prefix, "body_acc_y_",dir, ".txt", sep="") )
-	body_acc_x <- read.csv( paste(file_prefix, "body_acc_x_",dir, ".txt", sep="") )
-
-	x_data[,"body_acc_z"] <- body_acc_z[1]
-	x_data[,"body_acc_y"] <- body_acc_y[1]
-	x_data[,"body_acc_x"] <- body_acc_x[1]
-	
-	body_gyro_z <- read.csv( paste(file_prefix, "body_gyro_z_",dir, ".txt", sep="") )
-	body_gyro_y <- read.csv( paste(file_prefix, "body_gyro_y_",dir, ".txt", sep="") )
-	body_gyro_x <- read.csv( paste(file_prefix, "body_gyro_x_",dir, ".txt", sep="") )
-
-	x_data[,"body_gyro_z"] <- body_gyro_z[1]
-	x_data[,"body_gyro_y"] <- body_gyro_y[1]
-	x_data[,"body_gyro_x"] <- body_gyro_x[1]
-
-	alldata <- rbind(alldata, x_data)
+	## write the tidy data to a file
+	write.table(format(tidydata,digits=5),"tidydata.txt",sep=",",row.names=FALSE)
 }
-
-
 
